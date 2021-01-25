08ff5fce5023598ad0682c03d5d41d31489777b2
Zakaria Abdellatif <zakariahassan1995@gmail.com>
2021-01-25 20:55:09 +0200

    initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ img, setImage ] = useState(false)
+  const [ X, setX ] = useState(0)
+  const [ Y, setY ] = useState(0)
+  
+  const handleUpload = (e) => {
+    setImage(URL.createObjectURL(e.target.files[0]))
+  }
+  
+  const getXY = (e) => {
+    const Page = document.querySelector(".A4")
+    setX(e.pageX - Page.offsetLeft)
+    setY(e.pageY - Page.offsetTop)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="XY">
+      <div>
+        X : {X}
+        </div>
+      <div>
+        Y : {Y}
+      </div>
+
+      <div>
+        <input type="file" onChange={handleUpload}/>  
+      </div>
+      </div>
+      <div className="A4" onMouseMove={getXY}>
+        {img && <img style={{ width: "100%" }} src={img} alt="ddsad"/>}
+      </div> 
     </div>
   );
 }
